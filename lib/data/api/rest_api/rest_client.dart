9a29230ee9ed5aeca93ee518204a0f882f57e44d
Nikita Sadchenko <hekntsnv@gmail.com>
2022-04-16 00:13:52 +0300

    switched prod with sandbox

--- lib/data/api/rest_api/rest_client.dart
+++ lib/data/api/rest_api/rest_client.dart
@@ -5,7 +5,7 @@ import 'package:retrofit/retrofit.dart';
 
 part 'rest_client.g.dart';
 
-@RestApi(baseUrl: restApiProductionBaseUrl)
+@RestApi(baseUrl: restApiSandboxBaseUrl)
 abstract class RestClient {
   factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
 
